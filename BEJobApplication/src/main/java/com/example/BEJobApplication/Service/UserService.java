ba95c106305511e131667b9e28a3a5d4a7cc123c
TaiBui <buitaia9@gmail.com>
2025-04-21 02:03:10 +0700

    merge_fe_kt

--- BEJobApplication/src/main/java/com/example/BEJobApplication/Service/UserService.java
+++ BEJobApplication/src/main/java/com/example/BEJobApplication/Service/UserService.java
@@ -42,11 +42,12 @@ public class UserService {
     }
 
     public UserDTO getUserById(Integer id) {
-        User user= userRepository.findById(id)
+        User user = userRepository.findById(id)
                 .orElseThrow(() -> new NoFoundException("Không tìm thấy người dùng với ID: " + id));
-        return UserMapper.toUserDTO(user) ;
+        return UserMapper.toUserDTO(user);
     }
 //
+
     public Boolean deleteUser(Integer id) {
         if (!userRepository.existsById(id)) {
             throw new NoFoundException("Không tìm thấy người dùng với ID: " + id);
@@ -59,21 +60,26 @@ public class UserService {
         validateUser(userDTO);
         User user = UserMapper.toUserEntity(userDTO);
 //        user.setPassword("123456");
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
         user = userRepository.save(user);
-        return  UserMapper.toUserDTO(user);
+        return UserMapper.toUserDTO(user);
     }
-
 
     private void validateUser(UserCreateDTO user) {
         if (user.getUsername() == null || user.getUsername().isEmpty()) {
             throw new NoFoundException("Username không được để trống");
         }
+        String password = user.getPassword();
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z\\d])[A-Za-z\\d\\W]{8,}$";
+
+        if (password == null || !password.matches(regex)) {
+            throw new NoFoundException("Password phải có ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt");
+        }
         if (user.getEmail() == null || !user.getEmail().matches("^[\\w.-]+@[\\w.-]+\\.\\w{2,}$")) {
             throw new NoFoundException("Email không hợp lệ");
         }
-        if (user.getRole() == null ||
-                (!user.getRole().equalsIgnoreCase("ADMIN") && !user.getRole().equalsIgnoreCase("USER"))) {
+        if (user.getRole() == null
+                || (!user.getRole().equalsIgnoreCase("ADMIN") && !user.getRole().equalsIgnoreCase("USER"))) {
             throw new NoFoundException("Role phải là 'ADMIN' hoặc 'USER'");
         }
 
@@ -81,11 +87,12 @@ public class UserService {
             throw new NoFoundException("Email đã tồn tại");
         }
         String gender = user.getGender();
-        System.out.println("gender: "+gender);
+        System.out.println("gender: " + gender);
         if (gender == null || (!gender.equalsIgnoreCase("Nam") && !gender.equalsIgnoreCase("Nữ"))) {
             throw new NoFoundException("Giới tính phải là 'Nam' hoặc 'Nữ'");
         }
     }
+
     public UserDTO updateUser(Integer id, UserCreateDTO userDTO) {
         User existingUser = userRepository.findById(id)
                 .orElseThrow(() -> new NoFoundException("Không tìm thấy người dùng với ID: " + id));
